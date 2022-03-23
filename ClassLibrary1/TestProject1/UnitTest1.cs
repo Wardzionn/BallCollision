15411a6fe925b81de7c8da03d40fa98ecbdbe630
SzymonZiemecki <szymonj60@gmail.com>
2022-03-23 23:36:30 +0100

    Initial class and tests

--- ClassLibrary1/TestProject1/UnitTest1.cs
+++ ClassLibrary1/TestProject1/UnitTest1.cs
@@ -8,6 +8,11 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            ClassLibrary1.Class1 testowa = new ClassLibrary1.Class1(3,"esa","wariaty");
+            Assert.IsNotNull(testowa);
+            Assert.AreEqual(testowa.getAge(), 3);
+            testowa.setAge(5);
+            Assert.AreEqual(testowa.getAge(), 5);
         }
     }
 }
