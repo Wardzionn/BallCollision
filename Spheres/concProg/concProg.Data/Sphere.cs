606de1bfee989c9a28ea919dec6420ed892a39ff
Jakub Wardyn <236683@edu.p.lodz.pl>
2022-04-20 18:15:34 +0200

    prepared project structure

---
+++ Spheres/concProg/concProg.Data/Sphere.cs
@@ -0,0 +1,26 @@
+﻿namespace concProg.Data
+{
+    public class Sphere
+    {
+        int x;
+        int y;
+        int id { get; }
+
+        public Sphere(int spaceSize, int id)
+        {
+            Random rnd = new Random();
+            x = rnd.Next(0, spaceSize);
+            y = rnd.Next(0, spaceSize);
+            this.id = id;
+        }
+
+        public int[] getPos()
+        {
+            int[] position= new int[2];
+            position[0] = x;
+            position[1] = y;
+            return position;
+        }
+
+    }
+}
